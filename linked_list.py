d2e352310264f94ec8741e84e6aa7b81d6e1edb6
= <=>
2025-03-20 17:04:06 +0300

    increase size after adding using next

--- linked_list.py
+++ linked_list.py
@@ -1,8 +1,14 @@
 class LinkedList:
     def __init__(self):
-       self.size = 0
+       self.__size = 0
        self.head = None
        self.tail = None
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, increment):
+        self.__size += increment
     def __len__(self):
         return self.size
     def append(self, data):
@@ -23,19 +29,28 @@ class LinkedList:
             current = current.next
         print('None')
 class Node():
+    linked = LinkedList()
     def __init__(self, data):
-        self.next = None
+        self.__next = None
         self.data = data
+    @property
+    def next(self):
+        return self.__next
+    @next.setter
+    def next(self, node_a):
+        self.__next = node_a
+        self.linked.size += 1
+        self.linked.tail = node_a
     def __str__(self):
         return f"{self.data}"
 def main()-> None:
     node = Node(2)
     node_1 = Node(4)
-    node_2 = Node(5)
-    node_1.next = node_2
     linked_list = LinkedList()
     linked_list.append(node)
     linked_list.append(node_1)
+    node_2 = Node(5)
+    node_1.next = node_2
     print(len(linked_list))
     linked_list.display()
 
